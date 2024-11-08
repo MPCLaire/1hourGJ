53a1eb153b90ca9f2d3d91ee4c250716f571e9a4
Skyron <courtier.leo@gmail.com>
2024-11-08 14:23:34 +0100

    Prefab + Death

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] InputActionReference moveActionReference, hitActionReference;
     private InputAction MoveAction => moveActionReference.action;
     private InputAction HitAction => hitActionReference.action;
+    private Rigidbody2D _rb;
 
 
     private float _currentPos = -2;
@@ -15,6 +16,7 @@ public class PlayerController : MonoBehaviour
 
     private void Awake()
     {
+        _rb = GetComponent<Rigidbody2D>();
         MoveAction.Enable();
         HitAction.Enable();
         MoveAction.started += MoveActionOnstarted;
@@ -35,5 +37,24 @@ public class PlayerController : MonoBehaviour
     {
         _currentPos *= -1;
         transform.DOMoveX(_currentPos, moveDuration, true);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Object")) {
+            var obj = other.gameObject.GetComponent<IInteractable>();
+            obj.Interact(this);
+        }
+    }
+
+    public void Kill() {
+        _rb.gravityScale = 0;
+    }
+}
+
+public class Obstacle : MonoBehaviour, IInteractable
+{
+    public void Interact(PlayerController player) {
+        player.Kill();
     }
 }
